63e88d860ff869acb21aae8e5c6332c90ff15c3c
Brahyant-code <45344794+Brahyant-code@users.noreply.github.com>
2019-05-30 23:46:30 -0400

    MODIFICACION DE LAYOUT  PRODUCTO-V.2
    Se realizo cambio de botones de las cards y tambien  se agrego un slider(carrusel) al layout producto.
    
    tambien agregar que se realizo los tipos de secciones que implica (productos relacionados y lo mas vistos)

--- application/views/comprador/producto.php
+++ application/views/comprador/producto.php
@@ -1,19 +1,55 @@
 <div class="main-content shop-page main-content-detail">
 		<div class="container">
-			<div class="breadcrumbs">
-				<a href="#">Home</a> \ <span class="current">Detail</span>
-			</div>
+			 <br>
+			 	<div class="container-offset">
+					<!--Slideshow-->
+					<div class="main-slideshow slideshow1">
+						<div class="owl-carousel nav-style1" data-autoplay="true" data-nav="true" data-dots="false" data-loop="true" data-slidespeed="800" data-margin="30"  data-responsive = '{"0":{"items":1}, "640":{"items":1}, "768":{"items":1}, "1024":{"items":1}, "1200":{"items":1}}'>
+
+							<div class="slide-item item1">
+								<img class="img-responsive" src="<?php echo base_url();?>tools/images/slide18.jpg" alt="">
+								<div class="slide-content">
+									<h5 class="subtitle">hasta <span class="text-main-color">60% off</span> </h5>
+									<h3 class="title">Smart Watchs</h3>
+									<h5 class="smalltitle">Only: <span> $226.00</span></h5>
+									<a href="#" class="button">Comprar ahora</a>
+								</div>
+							</div>
+							<div class="slide-item item2">
+								<img src="<?php echo base_url();?>tools/images/slide18.jpg" alt="">
+								<div class="slide-content">
+									<h3 class="title"><span>Ventas</span> & Servicio de Laptop</h3>
+									<h4 class="subtitle">hasta <span class="text-main-color"> 50% off</span></h4>
+									<h5 class="smalltitle">Only: <span> $268.00</span></h5>
+									<a href="#" class="button">Comprar ahora</a>
+								</div>
+							</div>
+							<div class="slide-item item3">
+								<img src="<?php echo base_url();?>tools/images/slide18.jpg" alt="">
+								<div class="slide-content">
+									<h4 class="subtitle text-main-color">hasta 50% Off</h4>
+									<h3 class="title">Engranaje VR 3D.</h3>
+									<h5 class="smalltitle">La realidad virtual a través de una nueva lente.</h5>
+									<a href="#" class="button">Comprar ahora</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!--/Slideshow-->
+				</div>
+			
+		     	
 
 			<div class="products-arrivals">
 				<div class="section-head box-has-content">
-					<h4 class="section-title">Related Products</h4>
+					<h4 class="section-title">Productos Relacionados</h4>
 				</div>
 				<div class="section-content">
 					<div class="owl-carousel product-list-owl nav-style2 equal-container" data-autoplay="false" data-nav="true" data-dots="false" data-loop="false" data-slidespeed="800" data-margin="0"  data-responsive = '{"0":{"items":1}, "480":{"items":2}, "650":{"items":3}, "1024":{"items":4}, "1200":{"items":5}}'>
 	    				<div class="product-item layout1">
 							<div class="product-inner equal-elem">
 								<div class="thumb">
-									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span> Quick View</a>
+									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span>Vista rápida</a>
 									<a href="detail.html" class="thumb-link"><img src="<?php echo base_url();?>tools/images/product1.jpg" alt=""></a>
 								</div>
 								<div class="info">
@@ -24,9 +60,9 @@
 								</div>
 								<div class="group-button">
 									<div class="inner">
-										<a href="#" class="add-to-cart"><span class="text">ADD TO CART</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
-										<a href="#" class="compare-button"><i class="fa fa-exchange" aria-hidden="true"></i></a>
-										<a href="#" class="wishlist-button"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+										<a href="#" class="add-to-cart"><span class="text">Cotizar</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
+										<a href="#" class="compare-button"><i class="fa fa-commenting" aria-hidden="true"></i></a>
+										<a href="#" class="wishlist-button"><i class="fa fa-share-alt" aria-hidden="true"></i></a>
 									</div>
 								</div>
 							</div>
@@ -34,10 +70,10 @@
 						<div class="product-item layout1">
 							<div class="product-inner equal-elem">
 								<ul class="group-flash">
-									<li><span class="new flash">NEW</span></li>
+									<li><span class="new flash">Nuevo</span></li>
 								</ul>											
 								<div class="thumb">
-									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span> Quick View</a>
+									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span>Vista rápida</a>
 									<a href="detail.html" class="thumb-link"><img src="<?php echo base_url();?>tools/images/product4.jpg" alt=""></a>
 								</div>
 								<div class="info">
@@ -48,9 +84,9 @@
 								</div>
 								<div class="group-button">
 									<div class="inner">
-									<a href="#" class="add-to-cart"><span class="text">ADD TO CART</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
-									<a href="#" class="compare-button"><i class="fa fa-exchange" aria-hidden="true"></i></a>
-									<a href="#" class="wishlist-button"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+									<a href="#" class="add-to-cart"><span class="text">Cotizar</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
+									<a href="#" class="compare-button"><i class="fa fa-commenting" aria-hidden="true"></i></a>
+									<a href="#" class="wishlist-button"><i class="fa fa-share-alt" aria-hidden="true"></i></a>
 									</div>
 								</div>
 							</div>
@@ -59,10 +95,10 @@
 							<div class="product-inner equal-elem">
 								<ul class="group-flash">
 									<li><span class="sale flash">-50%</span></li>
-									<li><span class="best flash">Bestseller</span></li>
+									<li><span class="best flash">Mejor vendido</span></li>
 								</ul>
 								<div class="thumb">
-									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span> Quick View</a>
+									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span>Vista rápida</a>
 									<a href="detail.html" class="thumb-link"><img src="<?php echo base_url();?>tools/images/product5.jpg" alt=""></a>
 								</div>
 								<div class="info">
@@ -74,9 +110,9 @@
 								</div>
 								<div class="group-button">
 									<div class="inner">
-									<a href="#" class="add-to-cart"><span class="text">ADD TO CART</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
-									<a href="#" class="compare-button"><i class="fa fa-exchange" aria-hidden="true"></i></a>
-									<a href="#" class="wishlist-button"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+									<a href="#" class="add-to-cart"><span class="text">Cotizar</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
+									<a href="#" class="compare-button"><i class="fa fa-commenting" aria-hidden="true"></i></a>
+									<a href="#" class="wishlist-button"><i class="fa fa-share-alt" aria-hidden="true"></i></a>
 									</div>
 								</div>
 							</div>
@@ -84,7 +120,7 @@
 						<div class="product-item layout1">
 							<div class="product-inner equal-elem">
 								<div class="thumb">
-									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span> Quick View</a>
+									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span>Vista rápida</a>
 									<a href="detail.html" class="thumb-link"><img src="<?php echo base_url();?>tools/images/product6.jpg" alt=""></a>
 								</div>
 								<div class="info">
@@ -95,9 +131,9 @@
 								</div>
 								<div class="group-button">
 									<div class="inner">
-									<a href="#" class="add-to-cart"><span class="text">ADD TO CART</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
-									<a href="#" class="compare-button"><i class="fa fa-exchange" aria-hidden="true"></i></a>
-									<a href="#" class="wishlist-button"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+									<a href="#" class="add-to-cart"><span class="text">Cotizar</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
+									<a href="#" class="compare-button"><i class="fa fa-commenting" aria-hidden="true"></i></a>
+									<a href="#" class="wishlist-button"><i class="fa fa-share-alt" aria-hidden="true"></i></a>
 									</div>
 								</div>
 							</div>
@@ -108,7 +144,7 @@
 									<li><span class="sale flash">-50%</span></li>
 								</ul>
 								<div class="thumb">
-									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span> Quick View</a>
+									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span>Vista rápida</a>
 									<a href="detail.html" class="thumb-link"><img src="<?php echo base_url();?>tools/images/product7.jpg" alt=""></a>
 								</div>
 								<div class="info">
@@ -120,9 +156,9 @@
 								</div>
 								<div class="group-button">
 									<div class="inner">
-									<a href="#" class="add-to-cart"><span class="text">ADD TO CART</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
-									<a href="#" class="compare-button"><i class="fa fa-exchange" aria-hidden="true"></i></a>
-									<a href="#" class="wishlist-button"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+									<a href="#" class="add-to-cart"><span class="text">Cotizar</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
+									<a href="#" class="compare-button"><i class="fa fa-commenting" aria-hidden="true"></i></a>
+									<a href="#" class="wishlist-button"><i class="fa fa-share-alt" aria-hidden="true"></i></a>
 									</div>
 								</div>
 							</div>
@@ -131,10 +167,10 @@
 							<div class="product-inner equal-elem">
 								<ul class="group-flash">
 									<li><span class="sale flash">-50%</span></li>
-									<li><span class="best flash">Bestseller</span></li>
+									<li><span class="best flash">Mejor vendido</span></li>
 								</ul>
 								<div class="thumb">
-									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span> Quick View</a>
+									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span>Vista rápida</a>
 									<a href="detail.html" class="thumb-link"><img src="<?php echo base_url();?>tools/images/product24.jpg" alt=""></a>
 								</div>
 								<div class="info">
@@ -146,9 +182,9 @@
 								</div>
 								<div class="group-button">
 									<div class="inner">
-									<a href="#" class="add-to-cart"><span class="text">ADD TO CART</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
-									<a href="#" class="compare-button"><i class="fa fa-exchange" aria-hidden="true"></i></a>
-									<a href="#" class="wishlist-button"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+									<a href="#" class="add-to-cart"><span class="text">Cotizar</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
+									<a href="#" class="compare-button"><i class="fa fa-commenting" aria-hidden="true"></i></a>
+									<a href="#" class="wishlist-button"><i class="fa fa-share-alt" aria-hidden="true"></i></a>
 									</div>
 								</div>
 							</div>
@@ -156,7 +192,162 @@
 	    			</div>
 	    		</div>
 			</div>
-		</div>
+
+			<br>
+
+			<div class="products-arrivals">
+				<div class="section-head box-has-content">
+					<h4 class="section-title">Productos mas vistos</h4>
+				</div>
+				<div class="section-content">
+					<div class="owl-carousel product-list-owl nav-style2 equal-container" data-autoplay="false" data-nav="true" data-dots="false" data-loop="false" data-slidespeed="800" data-margin="0"  data-responsive = '{"0":{"items":1}, "480":{"items":2}, "650":{"items":3}, "1024":{"items":4}, "1200":{"items":5}}'>
+	    				<div class="product-item layout1">
+							<div class="product-inner equal-elem">
+								<div class="thumb">
+									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span>Vista rápida</a>
+									<a href="detail.html" class="thumb-link"><img src="<?php echo base_url();?>tools/images/product1.jpg" alt=""></a>
+								</div>
+								<div class="info">
+									<a href="detail.html" class="product-name">Rubberized Hard Case Older MacBook Pro 13.3"</a>
+									<div class="price">
+										<span>$350.00</span>
+									</div>
+								</div>
+								<div class="group-button">
+									<div class="inner">
+										<a href="#" class="add-to-cart"><span class="text">Cotizar</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
+										<a href="#" class="compare-button"><i class="fa fa-commenting" aria-hidden="true"></i></a>
+										<a href="#" class="wishlist-button"><i class="fa fa-share-alt" aria-hidden="true"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="product-item layout1">
+							<div class="product-inner equal-elem">
+								<ul class="group-flash">
+									<li><span class="new flash">Nuevo</span></li>
+								</ul>											
+								<div class="thumb">
+									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span>Vista rápida</a>
+									<a href="detail.html" class="thumb-link"><img src="<?php echo base_url();?>tools/images/product4.jpg" alt=""></a>
+								</div>
+								<div class="info">
+									<a href="detail.html" class="product-name">Smartphone RAM 4 GB New</a>
+									<div class="price">
+										<span >$350.00</span>
+									</div>
+								</div>
+								<div class="group-button">
+									<div class="inner">
+									<a href="#" class="add-to-cart"><span class="text">Cotizar</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
+									<a href="#" class="compare-button"><i class="fa fa-commenting" aria-hidden="true"></i></a>
+									<a href="#" class="wishlist-button"><i class="fa fa-share-alt" aria-hidden="true"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="product-item layout1">
+							<div class="product-inner equal-elem">
+								<ul class="group-flash">
+									<li><span class="sale flash">-50%</span></li>
+									<li><span class="best flash">Mejor vendido</span></li>
+								</ul>
+								<div class="thumb">
+									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span>Vista rápida</a>
+									<a href="detail.html" class="thumb-link"><img src="<?php echo base_url();?>tools/images/product5.jpg" alt=""></a>
+								</div>
+								<div class="info">
+									<a href="detail.html" class="product-name">Fujifilm INSTAX Mini 8 Instant Camera (White)</a>
+									<div class="price">
+										<span class="del">$700.00</span>
+										<span class="ins">$350.00</span>
+									</div>
+								</div>
+								<div class="group-button">
+									<div class="inner">
+									<a href="#" class="add-to-cart"><span class="text">Cotizar</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
+									<a href="#" class="compare-button"><i class="fa fa-commenting" aria-hidden="true"></i></a>
+									<a href="#" class="wishlist-button"><i class="fa fa-share-alt" aria-hidden="true"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="product-item layout1">
+							<div class="product-inner equal-elem">
+								<div class="thumb">
+									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span>Vista rápida</a>
+									<a href="detail.html" class="thumb-link"><img src="<?php echo base_url();?>tools/images/product6.jpg" alt=""></a>
+								</div>
+								<div class="info">
+									<a href="detail.html" class="product-name">PC Prox 21.5-inch and 27-inch (Late 2018) reviews</a>
+									<div class="price">
+										<span >$550.00</span>
+									</div>
+								</div>
+								<div class="group-button">
+									<div class="inner">
+									<a href="#" class="add-to-cart"><span class="text">Cotizar</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
+									<a href="#" class="compare-button"><i class="fa fa-commenting" aria-hidden="true"></i></a>
+									<a href="#" class="wishlist-button"><i class="fa fa-share-alt" aria-hidden="true"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="product-item layout1">
+							<div class="product-inner equal-elem">
+								<ul class="group-flash">
+									<li><span class="sale flash">-50%</span></li>
+								</ul>
+								<div class="thumb">
+									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span>Vista rápida</a>
+									<a href="detail.html" class="thumb-link"><img src="<?php echo base_url();?>tools/images/product7.jpg" alt=""></a>
+								</div>
+								<div class="info">
+									<a href="detail.html" class="product-name">Best Accessories- SteelSeries NIMBUS Controlle</a>
+									<div class="price">
+										<span class="del">$500.00</span>
+										<span class="ins">$250.00</span>
+									</div>
+								</div>
+								<div class="group-button">
+									<div class="inner">
+									<a href="#" class="add-to-cart"><span class="text">Cotizar</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
+									<a href="#" class="compare-button"><i class="fa fa-commenting" aria-hidden="true"></i></a>
+									<a href="#" class="wishlist-button"><i class="fa fa-share-alt" aria-hidden="true"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="product-item layout1">
+							<div class="product-inner equal-elem">
+								<ul class="group-flash">
+									<li><span class="sale flash">-50%</span></li>
+									<li><span class="best flash">Mejor vendido</span></li>
+								</ul>
+								<div class="thumb">
+									<a href="#" class="quickview-button"><span class="icon"><i class="fa fa-eye" aria-hidden="true"></i></span>Vista rápida</a>
+									<a href="detail.html" class="thumb-link"><img src="<?php echo base_url();?>tools/images/product24.jpg" alt=""></a>
+								</div>
+								<div class="info">
+									<a href="detail.html" class="product-name">Fujifilm INSTAX Mini 8 Instant Camera (White)</a>
+									<div class="price">
+										<span class="del">$700.00</span>
+										<span class="ins">$350.00</span>
+									</div>
+								</div>
+								<div class="group-button">
+									<div class="inner">
+									<a href="#" class="add-to-cart"><span class="text">Cotizar</span><span class="icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></span></a>
+									<a href="#" class="compare-button"><i class="fa fa-commenting" aria-hidden="true"></i></a>
+									<a href="#" class="wishlist-button"><i class="fa fa-share-alt" aria-hidden="true"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+	    			</div>
+	    		</div>
+			</div>
+		
 
 		<div class="brand">
 			<div class="container">
@@ -169,4 +360,5 @@
 				</div>
 			</div>
 		</div>
-	</div>
+  </div>
+</div>
