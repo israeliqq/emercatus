b6b4a0aeba97d0861a4af57b245e56bda7fee56a
israeliqq <israeliqq@live.cl>
2019-06-04 18:47:51 -0400

    DIMENSION MIS PRODUCTOS
    DIMENSION TABLET PHONE NOTEBOOK DESKTOP

--- application/views/vendedor/misproductos.php
+++ application/views/vendedor/misproductos.php
@@ -1,99 +1,114 @@
 <div class="container">
     <div class="row">
-        <div class="well">
+
         <h1 class="text-center" style="font-family: 'Open Sans'">Mis Productos</h1>
         <div class="list-group">
-          <a href="#" class="list-group-item ">
-                <div class="media col-md-3">
-                    <figure class="pull-left">
-                        <img class="media-object img-rounded img-responsive"  src="http://placehold.it/350x250" alt="placehold.it/350x250" >
-                    </figure>
-                </div>
-                <div class="col-md-6">
-                    <h4 class="list-group-item-heading"> Descripción </h4>
-                    <p class="list-group-item-text"> Qui diam libris ei, vidisse incorrupte at mel. His euismod salutandi dissentiunt eu. Habeo offendit ea mea. Nostro blandit sea ea, viris timeam molestiae an has. At nisl platonem eum. 
-                        Vel et nonumy gubergren, ad has tota facilis probatus. Ea legere legimus tibique cum, sale tantas vim ea, eu vivendo expetendis vim. Voluptua vituperatoribus et mel, ius no elitr deserunt mediocrem. Mea facilisi torquatos ad.
-
-                    </p>
-                    <br>
-                        <button type="" class="btn btn-dafault " style="background-color: transparent;border-color: transparent;color:#0A7BC1;">Modificar</button>
-                        <span class="fa fa-calendar"> 18/04/2019  </span>
-                        <span class="fa fa-clock-o"> 17:09   </span>
-                    
-                </div>
-                <div class="col-md-3 text-center">
-                    <h2><span class="fa fa-eye"></span> 12424 <small> vistos </small></h2>
-                   
-                    <button type="button" class="btn btn-danger btn-lg btn-block">Eliminar</button>
-                    <div class="stars">
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>
-                    </div>
-                    <p> Promedio 4.5 <small> / </small> 5 </p>
-                </div>
-          </a>
-          <a href="#" class="list-group-item">
-                <div class="media col-md-3">
-                    <figure class="pull-left">
-                        <img class="media-object img-rounded img-responsive" src="http://placehold.it/350x250" alt="placehold.it/350x250" >
-                    </figure>
-                </div>
-                <div class="col-md-6">
-                    <h4 class="list-group-item-heading"> Descripción</h4>
-                    <p class="list-group-item-text"> Eu eum corpora torquatos, ne postea constituto mea, quo tale lorem facer no. Ut sed odio appetere partiendo, quo meliore salutandi ex. Vix an sanctus vivendo, sed vocibus accumsan petentium ea. 
-                        Sed integre saperet at, no nec debet erant, quo dico incorrupte comprehensam ut. Et minimum consulatu ius, an dolores iracundia est, oportere vituperata interpretaris sea an. Sed id error quando indoctum, mel suas saperet at.                         
-                    </p>
-                    <br>
-                     <button type="" class="btn btn-dafault " style="background-color: transparent;border-color: transparent;color:#0A7BC1;">Modificar</button>
-                        <span class="fa fa-calendar"> 18/04/2019  </span>
-                        <span class="fa fa-clock-o"> 17:09   </span>
-                </div>
-                <div class="col-md-3 text-center">
-                    <h2><span class="fa fa-eye"></span> 12424 <small> vistos </small></h2>
-                    <button type="button" class="btn btn-danger btn-lg btn-block">Eliminar</button>
-                    <div class="stars">
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>
-                    </div>
-                    <p> Promedio 3.9 <small> / </small> 5 </p>
-                </div>
-          </a>
-          <a href="#" class="list-group-item">
-                <div class="media col-md-3">
-                    <figure class="pull-left">
-                        <img class="media-object img-rounded img-responsive" src="http://placehold.it/350x250" alt="placehold.it/350x250">
-                    </figure>
-                </div>
-                <div class="col-md-6">
-                    <h4 class="list-group-item-heading">  Descripción </h4>
-                    <p class="list-group-item-text"> Ut mea viris eripuit theophrastus, cu ponderum accusata consequuntur cum. Suas quaestio cotidieque pro ea. Nam nihil persecuti philosophia id, nam quot populo ea. 
-                        Falli urbanitas ei pri, eu est enim volumus, mei no volutpat periculis. Est errem iudicabit cu. At usu vocibus officiis, ad ius eros tibique appellantur.                         
-                    </p>
-                    <br>
-                     <button type="" class="btn btn-dafault " style="background-color: transparent;border-color: transparent;color:#0A7BC1;">Modificar</button>
-                        <span class="fa fa-calendar"> 18/04/2019  </span>
-                        <span class="fa fa-clock-o"> 17:09   </span>
-                </div>
-                <div class="col-md-3 text-center">
-                    <h2><span class="fa fa-eye"></span> 12424 <small> vistos </small></h2>
-                    <button type="button" class="btn btn-danger btn-lg btn-block">Eliminar</button>
-                    <div class="stars">
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>
-                    </div>
-                    <p> Promedio 4.1 <small> / </small> 5 </p>
-                </div>
-          </a>
-        </div>
-        </div>
-    </div>
+		
+		
+		
+		
+		
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 list-group-item">
+				<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 text-center">
+					<figure class="pull-center">
+						<img class="media-object img-rounded img-responsive center-block" src="http://placehold.it/350x250" alt="placehold.it/350x250">
+					</figure>
+					<br>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6  ">
+					<h4 class="list-group-item-heading"> Descripción </h4>
+					<p class="list-group-item-text"> Qui diam libris ei, vidisse incorrupte at mel. His euismod salutandi dissentiunt eu. Habeo offendit ea mea. Nostro blandit sea ea, viris timeam molestiae an has. At nisl platonem eum. Vel et nonumy gubergren, ad has tota facilis probatus. Ea legere legimus tibique cum, sale tantas vim ea, eu vivendo expetendis vim. Voluptua vituperatoribus et mel, ius no elitr deserunt mediocrem. Mea facilisi torquatos ad.
+					</p>
+					<br>
+					<a href="" type="submit">Modificar</a>
+					<span class="fa fa-calendar"></span> 18/04/2019
+					<span class="fa fa-clock-o"></span> 17:09  
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3  text-center">
+					<h2><span class="fa fa-eye"></span> 12424 <small> vistos </small></h2>
+					<button type="button" class="btn btn-danger btn-lg btn-block">Eliminar</button>
+					<div class="stars">
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+					</div>
+					<p> Promedio 4.5 <small> / </small> 5 </p>
+				</div>
+			</div>
+			
+			
+			            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 list-group-item">
+				<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 text-center">
+					<figure class="pull-center">
+						<img class="media-object img-rounded img-responsive center-block" src="http://placehold.it/350x250" alt="placehold.it/350x250">
+					</figure>
+					<br>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6  ">
+					<h4 class="list-group-item-heading"> Descripción </h4>
+					<p class="list-group-item-text"> Qui diam libris ei, vidisse incorrupte at mel. His euismod salutandi dissentiunt eu. Habeo offendit ea mea. Nostro blandit sea ea, viris timeam molestiae an has. At nisl platonem eum. Vel et nonumy gubergren, ad has tota facilis probatus. Ea legere legimus tibique cum, sale tantas vim ea, eu vivendo expetendis vim. Voluptua vituperatoribus et mel, ius no elitr deserunt mediocrem. Mea facilisi torquatos ad.
+					</p>
+					<br>
+					<a href="" type="submit">Modificar</a>
+					<span class="fa fa-calendar"></span> 18/04/2019
+					<span class="fa fa-clock-o"></span> 17:09  
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3  text-center">
+					<h2><span class="fa fa-eye"></span> 12424 <small> vistos </small></h2>
+					<button type="button" class="btn btn-danger btn-lg btn-block">Eliminar</button>
+					<div class="stars">
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+					</div>
+					<p> Promedio 4.5 <small> / </small> 5 </p>
+				</div>
+			</div>
+			
+			
+			
+			            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 list-group-item">
+				<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 text-center">
+					<figure class="pull-center">
+						<img class="media-object img-rounded img-responsive center-block" src="http://placehold.it/350x250" alt="placehold.it/350x250">
+					</figure>
+					<br>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6  ">
+					<h4 class="list-group-item-heading"> Descripción </h4>
+					<p class="list-group-item-text"> Qui diam libris ei, vidisse incorrupte at mel. His euismod salutandi dissentiunt eu. Habeo offendit ea mea. Nostro blandit sea ea, viris timeam molestiae an has. At nisl platonem eum. Vel et nonumy gubergren, ad has tota facilis probatus. Ea legere legimus tibique cum, sale tantas vim ea, eu vivendo expetendis vim. Voluptua vituperatoribus et mel, ius no elitr deserunt mediocrem. Mea facilisi torquatos ad.
+					</p>
+					<br>
+					<a href="" type="submit">Modificar</a>
+					<span class="fa fa-calendar"></span> 18/04/2019
+					<span class="fa fa-clock-o"></span> 17:09  
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3  text-center">
+					<h2><span class="fa fa-eye"></span> 12424 <small> vistos </small></h2>
+					<button type="button" class="btn btn-danger btn-lg btn-block">Eliminar</button>
+					<div class="stars">
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+					</div>
+					<p> Promedio 4.5 <small> / </small> 5 </p>
+				</div>
+			</div>
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		</div>
+	</div>
 </div>
